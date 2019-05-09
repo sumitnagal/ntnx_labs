49aa0377fb4d4cab67c885bc17a0a90ac0721a46
gopherland <fernand@gopherland.io>
2019-05-09 13:48:52 -0600

    updates

---
+++ documentation/fizzbuzz/game.go
@@ -0,0 +1,10 @@
+// © 2019 Imhotep Software LLC. All rights reserved.
+
+// Package fizzbuzz implements a FizzBuzz calculator.
+package fizzbuzz
+
+
+// !!YOUR_CODE!!
+func Compute(n int) string {
+	// !!YOUR_CODE!!
+}
---
+++ documentation/fizzbuzz/game_test.go
@@ -0,0 +1,34 @@
+// © 2019 Imhotep Software LLC. All rights reserved.
+
+package fizzbuzz_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var Output string
+
+func TestCompute(t *testing.T) {
+	// !!YOUR_CODE!!
+}
+
+func BenchmarkCompute(b *testing.B) {
+	// !!YOUR_CODE!!
+}
+
+// !!YOUR_CODE!!
+func ExampleCompute_DivisibleBy3And5() {
+	// !!YOUR_CODE!!
+}
+
+// !!YOUR_CODE!!
+func ExampleCompute_DivisibleBy3() {
+	// !!YOUR_CODE!!
+}
+
+// !!YOUR_CODE!!
+func ExampleCompute_DivisibleBy5() {
+	// !!YOUR_CODE!!
+}
